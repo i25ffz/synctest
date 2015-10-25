e3edf6eeb93c1b854763f10e4ef7a19312b08ccb
Martin Foo <i25ffz@gmail.com>
2015-10-25 12:45:33 +0800

    fix linux support.

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ mingw32-g++ -shared -Wl,--kill-at -O2 -s -static -ID:/jdk1.7.0_55/include -ID:/j
 mingw32-g++ -shared -Wl,--kill-at -O2 -s -static -ID:/jdk1.7.0_55/include -ID:/jdk1.7.0_55/include/win32 -o mutex_single.dll testmutex.cpp
 
 ls -l *.dll
+
+g++ -shared -fPIC -I/usr/lib/jvm/java-7-oracle/include -I/usr/lib/jvm/java-7-oracle/include/linux -O2 -s -DMULTI=1 -o libmutex_multi.so testmutex.cpp
+g++ -shared -fPIC -I/usr/lib/jvm/java-7-oracle/include -I/usr/lib/jvm/java-7-oracle/include/linux -O2 -s -o libmutex_single.so testmutex.cpp
+
+ls -l *.so
